dc112e708a7779d9319a6df8b6f27eb35c3cf3cc
christiano39 <carneson39@gmail.com>
2020-12-16 16:16:58 -0700

    increase json body limit and add words routes

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const bodyParser = require("body-parser");
+
+const wordsRoutes = require("../words/wordsRoutes");
+
+const JSON_SIZE_LIMIT = "5mb";
 
 const server = express();
 
+server.use(bodyParser.json({ limit: JSON_SIZE_LIMIT }));
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
@@ -11,5 +17,7 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up", timestamp: Date.now() });
 });
+
+server.use("/api/words", wordsRoutes);
 
 module.exports = server;
