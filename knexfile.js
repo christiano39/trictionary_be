76d83cef1492e537d894eae0d4136203637e2d3b
christiano39 <carneson39@gmail.com>
2020-12-16 16:20:08 -0700

    add pool size for development

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,22 @@
-const dotenv = require("dotenv");
-dotenv.config();
+var dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 
 module.exports = {
   development: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
+    pool: {
+      min: 2,
+      max: 10,
     },
   },
+
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
   },
 };
