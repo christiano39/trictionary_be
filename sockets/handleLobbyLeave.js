eb3db86fd916630903a514c63bdddb69aee0eb20
christiano39 <carneson39@gmail.com>
2020-12-18 13:58:54 -0700

    players are removed from game state when disconnecting

---
+++ sockets/handleLobbyLeave.js
@@ -0,0 +1,21 @@
+module.exports = handleLobbyLeave;
+
+function handleLobbyLeave(io, socket, lobbies) {
+  if (Array.from(socket.rooms).length > 1) {
+    const lobbyCode = Array.from(socket.rooms)[1];
+
+    socket.leave(lobbyCode);
+
+    lobbies[lobbyCode].players = lobbies[lobbyCode].players.filter((player) => {
+      if (player.id === socket.id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
+
+    console.log(lobbies[lobbyCode]);
+  }
+}
