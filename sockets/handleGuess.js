4b655b06fe84c3facc9c4fc3dd46482b19f0a570
christiano39 <carneson39@gmail.com>
2020-12-19 10:51:29 -0700

    calculate points once everyone has guessed

--- sockets/handleGuess.js
+++ sockets/handleGuess.js
@@ -11,9 +11,25 @@ function handleGuess(io, socket, lobbyCode, guess, lobbies) {
       ...lobbies[lobbyCode],
       completed: true,
     };
+
+    lobbies[lobbyCode] = calculatePoints(lobbies[lobbyCode]);
   }
 
   io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
 
-  console.log(lobbyCode);
+  console.log(lobbies[lobbyCode]);
+}
+
+function calculatePoints(lobby) {
+  lobby.guesses.forEach((guess) => {
+    lobby.players.forEach((player) => {
+      if (guess.guess === "0" && player.id === guess.player) {
+        player.points++;
+      } else if (guess.guess === player.id && guess.player !== player.id) {
+        player.points++;
+      }
+    });
+  });
+
+  return lobby;
 }
