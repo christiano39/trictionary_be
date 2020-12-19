8f375236490f720b55f0e61fd60c245b0cf8e552
christiano39 <carneson39@gmail.com>
2020-12-19 13:00:17 -0700

    automatically advance gamestate once everyone has submitted definitions

--- sockets/handleSubmitDefinition.js
+++ sockets/handleSubmitDefinition.js
@@ -1,13 +1,26 @@
 module.exports = handleSubmitDefinition;
 
 function handleSubmitDefinition(io, socket, definition, lobbyCode, lobbies) {
+  let numSubmitted = 0;
+
   lobbies[lobbyCode].players = lobbies[lobbyCode].players.map((player) => {
+    if (player.definition) {
+      numSubmitted++;
+    }
+
     if (player.id === socket.id) {
       return { ...player, definition };
     } else {
       return player;
     }
   });
+
+  if (numSubmitted === lobbies[lobbyCode].players.length - 1) {
+    lobbies[lobbyCode] = {
+      ...lobbies[lobbyCode],
+      guessing: true,
+    };
+  }
 
   io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
 
