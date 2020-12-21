af73049c7e2e0ead1193c1a2f5451dac19286032
christiano39 <carneson39@gmail.com>
2020-12-21 10:21:04 -0700

    add some crash protection when attempting to join an invalid lobby

--- sockets/handleLobbyJoin.js
+++ sockets/handleLobbyJoin.js
@@ -7,13 +7,15 @@ function handleLobbyJoin(io, socket, username, lobbyCode, lobbies) {
 
   socket.join(lobbyCode);
 
-  lobbies[lobbyCode] = {
-    ...lobbies[lobbyCode],
-    players: [
-      ...lobbies[lobbyCode].players,
-      { id: socket.id, username, definition: "", points: 0 },
-    ],
-  };
+  if (lobbies[lobbyCode] && lobbies[lobbyCode].players) {
+    lobbies[lobbyCode] = {
+      ...lobbies[lobbyCode],
+      players: [
+        ...lobbies[lobbyCode].players,
+        { id: socket.id, username, definition: "", points: 0 },
+      ],
+    };
+  }
 
   io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
 
