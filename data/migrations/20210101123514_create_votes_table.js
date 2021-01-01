6b108c497ab8853025e7b6b665b53b3e1f409f98
christiano39 <carneson39@gmail.com>
2021-01-01 12:40:48 -0700

    add migration to create Votes table

---
+++ data/migrations/20210101123514_create_votes_table.js
@@ -0,0 +1,26 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("Votes", (tbl) => {
+    tbl.increments("id");
+
+    tbl.string("user_id", 255).notNullable();
+
+    tbl
+      .integer("definition_id")
+      .unsigned()
+      .references("Definitions.id")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+
+    tbl
+      .integer("round_id")
+      .unsigned()
+      .notNullable()
+      .references("Rounds.id")
+      .onDelete("RESTRICT")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("Votes");
+};
