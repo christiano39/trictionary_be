dab4a376963725926c3c7441640d08eddf238132
christiano39 <carneson39@gmail.com>
2021-01-04 11:28:19 -0700

    create round in db on round start

--- sockets/handleStartGame.js
+++ sockets/handleStartGame.js
@@ -1,4 +1,5 @@
 const Words = require("../words/wordsModel");
+const Rounds = require("../rounds/roundsModel");
 
 module.exports = handleStartGame;
 
@@ -8,16 +9,23 @@ function handleStartGame(io, socket, lobbyCode, lobbies) {
       const index = Math.floor(Math.random() * possibleWords.length);
       const word = possibleWords[index];
 
-      lobbies[lobbyCode] = {
-        ...lobbies[lobbyCode],
-        phase: "WRITING",
-        word: word.word,
-        definition: word.definition,
-      };
+      Rounds.add(lobbies[lobbyCode], word.id)
+        .then(([roundId]) => {
+          lobbies[lobbyCode] = {
+            ...lobbies[lobbyCode],
+            phase: "WRITING",
+            word: word.word,
+            definition: word.definition,
+            roundId,
+          };
 
-      io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
+          io.to(lobbyCode).emit("game update", lobbies[lobbyCode]);
 
-      console.log(lobbies[lobbyCode]);
+          console.log(lobbies[lobbyCode]);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
     })
     .catch((err) => {
       console.log(err.message);
